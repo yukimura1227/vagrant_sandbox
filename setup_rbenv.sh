08e1c9b4ce63e4ec1b303ff3549b3533b0f5e4f8
vagrant <vagrant@localhost.localdomain>
2016-02-11 03:32:51 +0000

    rbenv setup script

---
+++ setup_rbenv.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+RBENV_INSTALL_DEST_DIR=/usr/local/rbenv
+COMMON_BASH_SETTING_FILE=/etc/profile
+
+echo "install gcc..."
+sudo yum -y install gcc
+
+echo "install openssl-devel..."
+sudo yum -y install openssl-devel
+
+# TODO chcke git installed. and install git if git not installed
+#echo "install git..."
+#sudo yum -y install git
+
+echo "download rbenv..."
+git clone https://github.com/sstephenson/rbenv.git ${RBENV_INSTALL_DEST_DIR}
+
+# add rbenv path To $PATH
+echo "append rbenv path info To ${COMMON_BASH_SETTING_FILE}..."
+echo "export RBENV_ROOT=\"${RBENV_INSTALL_DEST_DIR}\"" >> ${COMMON_BASH_SETTING_FILE}
+echo 'export PATH="${RBENV_ROOT}/bin:$PATH"' >> ${COMMON_BASH_SETTING_FILE}
+echo 'eval "$(rbenv init -)"' >> ${COMMON_BASH_SETTING_FILE}
+
+# ${COMMON_BASH_SETTING_FILE} reload
+echo "${COMMON_BASH_SETTING_FILE} reload"
+source ${COMMON_BASH_SETTING_FILE}
+
+echo "download rbenv plugin(ruby-build)..."
+git clone https://github.com/sstephenson/ruby-build.git ${RBENV_INSTALL_DEST_DIR}/plugins/ruby-build
+
+# install ruby
+echo "ruby install..."
+rbenv install -v 2.1.2
+
+rbenv global 2.1.2
+
+rbenv rehash
+
+# set rbenv owner
+chown -R vagrant:vagrant ${RBENV_INSTALL_DEST_DIR}
