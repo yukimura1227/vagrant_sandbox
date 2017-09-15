63c2e54d846acdc964c4d7b0fb3af20358933efe
yukimura1227 <takamura1227@gmail.com>
2017-09-15 15:50:24 +0900

    fix git version

--- utils/setup_git.sh
+++ utils/setup_git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GIT_TARGET_VERSION="git-2.7.0"
+GIT_TARGET_VERSION="git-2.14.1"
 ARCHIVED_GIT_FILE_NAME="${GIT_TARGET_VERSION}.tar.gz"
 GIT_URL="https://www.kernel.org/pub/software/scm/git/${ARCHIVED_GIT_FILE_NAME}"
 
