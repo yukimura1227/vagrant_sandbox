c7b6e545589516541f872fc2ff704bb3104a9cd8
yukimura1227 <takamura1227@gmail.com>
2017-09-17 14:42:27 +0900

    fix vim setup script using git instad of mercurial

--- utils/setup_vim.sh
+++ utils/setup_vim.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
+which git
+if [ $? -ne 0 ]
+then
+  echo "install git..."
+  sudo yum -y install git
+fi
 
-sudo yum install mercurial -y
+which gcc
+if [ $? -ne 0 ]
+then
+  echo 'install gcc'
+  sudo yum -y install gcc
+fi
 
-sudo yum install ncurses-devel -y
+yum list installed | grep ncurses-devel
+if [ $? -eq 1 ]
+then
+  echo "install ncurses-devel and it's dependencies"
+  sudo yum -y install ncurses-devel
+  sudo yum -y install libXt-devel libX11-devel atk-devel gtk2-devel
+fi
 
 cd /usr/local/src
 
-hg clone https://vim.googlecode.com/hg/ vim
+git clone https://github.com/vim/vim.git
 
-cd /usr/local/src/vim
+cd /usr/local/src/vim/src
 
 ./configure --enable-multibyte --with-features=huge --disable-selinux --prefix=/usr/local
 
