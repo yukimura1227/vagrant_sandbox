1696a20f7e02bbf78cdfefef0ed91a1a3e30cbfb
yukimura1227 <takamura1227@gmail.com>
2017-10-10 08:26:08 +0900

    install phantomjs

--- utils/make_ready4rails.sh
+++ utils/make_ready4rails.sh
@@ -11,6 +11,12 @@ sudo yum install sqlite-devel -y
 echo "installing gcc-c++..."
 sudo yum install gcc-c++ -y
 
+echo "installing phantmjs..."
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar jxfv phantomjs-2.1.1-linux-x86_64.tar.bz2
+mv phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/
+sudo yum -y install fontconfig-devel
+
 echo "${GEM_PATH} install rails..."
 su - vagrant -c "${GEM_PATH} install --no-document rails -v '5.1.3'"
 
